28623ddb8efbb96b5e5727c32270f98bcc812fb6
Snehaittoop <snehaittoop868@gmail.com>
2022-08-08 15:35:30 +0530

    added

--- menu.py
+++ menu.py
@@ -1,10 +1,21 @@
+from re import sub
+
+
 def addition(num1,num2):
     res=num1+num2
     return res
-
+def sub(num1,num2):
+    res=num1-num2
+    return res
+def mul(num1,num2):
+    res=num1*num2
+    return res
+def div(num1,num2):
+    res=num1/num2
+    return res
 
 while True:
-    print("1. add two number: ")
+    print("1. add : ")
     print("2. substract 2 numbers: ")
     print("3. multiply 2 number: ")
     print("4. divide 2 numbers: ")
@@ -18,17 +29,17 @@ while True:
     elif choice==2:
         num1=int(input("enter first number"))
         num2=int(input("enter second number"))
-        result=addition(num1,num2)
+        result=sub(num1,num2)
         print(result)
     elif choice==3:
         num1=int(input("enter first number"))
         num2=int(input("enter second number"))
-        result=addition(num1,num2)
+        result=mul(num1,num2)
         print(result)
     elif choice==4:
         num1=int(input("enter first number"))
         num2=int(input("enter second number"))
-        result=addition(num1,num2)
+        result=div(num1,num2)
         print(result)
     else:
         break
